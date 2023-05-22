0935353e0db32a52d8c442dcef3084c8321d25e6
dosart <Dysprosium92@ya.ru>
2023-05-22 20:31:59 +0300

    refactor: add comments

--- src/opcodes.cpp
+++ src/opcodes.cpp
@@ -30,28 +30,53 @@ static uint8_t make_kk(uint16_t opcode) { return (opcode & 0x00FFu); }
 
 static uint16_t make_nnn(uint16_t opcode) { return (opcode & 0x0FFFu); }
 
-void op_00E0(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Clear the display.
+ */
+static void op_00E0(chip8_t *chip8) {
   std::memset(chip8->video, 0, sizeof(chip8->video));
 }
 
-void op_00EE(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Return from a subroutine.
+ */
+static void op_00EE(chip8_t *chip8) {
   chip8->sp -= 1;
   chip8->pc = chip8->stack[chip8->sp];
 }
 
-void op_1nnn(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Jump to location nnn.
+ */
+static void op_1nnn(chip8_t *chip8) {
   uint16_t nnn = make_nnn(chip8->opcode);
   chip8->pc = nnn;
 }
 
-void op_2nnn(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Call subroutine at nnn.
+ */
+static void op_2nnn(chip8_t *chip8) {
   uint16_t nnn = make_nnn(chip8->opcode);
   chip8->stack[chip8->sp] = chip8->pc;
   chip8->sp += 1;
   chip8->pc = nnn;
 }
 
-void op_3xkk(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Skip next instruction if vx = kk.
+ */
+static void op_3xkk(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t kk = make_kk(chip8->opcode);
 
@@ -59,7 +84,12 @@ void op_3xkk(chip8_t *chip8) {
     chip8->pc += 2;
 }
 
-void op_4xkk(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Skip next instruction if vx != kk.
+ */
+static void op_4xkk(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t kk = make_kk(chip8->opcode);
 
@@ -67,7 +97,12 @@ void op_4xkk(chip8_t *chip8) {
     chip8->pc += 2;
 }
 
-void op_5xy0(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Skip next instruction if vx = vy.
+ */
+static void op_5xy0(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
@@ -75,49 +110,74 @@ void op_5xy0(chip8_t *chip8) {
     chip8->pc += 2;
 }
 
-void op_6xkk(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = kk.
+ */
+static void op_6xkk(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t kk = make_kk(chip8->opcode);
 
   chip8->registers[vx] = kk;
 }
 
-void op_7xkk(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vx + kk.
+ */
+static void op_7xkk(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t kk = make_kk(chip8->opcode);
 
   chip8->registers[vx] += kk;
 }
 
-void op_8xy0(chip8_t *chip8) {
+static void op_8xy0(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
   chip8->registers[vx] = chip8->registers[vy];
 }
 
-void op_8xy1(chip8_t *chip8) {
+static void op_8xy1(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
   chip8->registers[vx] |= chip8->registers[vy];
 }
 
-void op_8xy2(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vy.
+ */
+static void op_8xy2(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
   chip8->registers[vx] &= chip8->registers[vy];
 }
 
-void op_8xy3(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vx OR vy.
+ */
+static void op_8xy3(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
   chip8->registers[vx] ^= chip8->registers[vy];
 }
 
-void op_8xy4(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vx + vy, set vf = carry.
+ */
+static void op_8xy4(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
@@ -131,7 +191,14 @@ void op_8xy4(chip8_t *chip8) {
   chip8->registers[vx] = sum & 0xFFu;
 }
 
-void op_8xy5(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vx - vy, set vf = NOT borrow.
+ * If vx > vy, then vf is set to 1, otherwise 0. Then vy is subtracted from vx,
+ * and the results stored in vx.
+ */
+static void op_8xy5(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
@@ -143,13 +210,27 @@ void op_8xy5(chip8_t *chip8) {
   chip8->registers[vx] -= chip8->registers[vy];
 }
 
-void op_8xy6(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vx - vy, set vf = NOT borrow.
+ * If vx > vy, then vf is set to 1, otherwise 0. Then vy is subtracted from vx,
+ * and the results stored in vx.
+ */
+static void op_8xy6(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   chip8->registers[0xF] = (chip8->registers[vx] & 0x1u);
   chip8->registers[vx] >>= 1;
 }
 
-void op_8xy7(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vy - vx, set vf = NOT borrow.
+ * If vy > vx, then vf is set to 1, otherwise 0. Then vx is subtracted from vy,
+ * and the results stored in vx.
+ */
+static void op_8xy7(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
@@ -161,14 +242,26 @@ void op_8xy7(chip8_t *chip8) {
   chip8->registers[vx] = chip8->registers[vy] - chip8->registers[vx];
 }
 
-void op_8xyE(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = vx SHL 1.
+ * If the most-significant bit of vx is 1, then vf is set to 1, otherwise to 0.
+ * Then vx is multiplied by 2.
+ */
+static void op_8xyE(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   chip8->registers[0xF] = (chip8->registers[vx] & 0x80u) >> 7u;
 
   chip8->registers[vx] <<= 1;
 }
 
-void op_9xy0(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Skip next instruction if vx != vy.
+ */
+static void op_9xy0(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t vy = make_vy(chip8->opcode);
 
@@ -176,17 +269,32 @@ void op_9xy0(chip8_t *chip8) {
     chip8->pc += 2;
 }
 
-void op_Annn(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set I = nnn.
+ */
+static void op_Annn(chip8_t *chip8) {
   uint16_t nnn = make_nnn(chip8->opcode);
   chip8->index = nnn;
 }
 
-void op_Bnnn(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Jump to location nnn + v0.
+ */
+static void op_Bnnn(chip8_t *chip8) {
   uint16_t nnn = make_nnn(chip8->opcode);
   chip8->pc = chip8->registers[0] + nnn;
 }
 
-void op_Cxkk(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set vx = random byte AND kk.
+ */
+static void op_Cxkk(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t kk = make_kk(chip8->opcode);
 
@@ -223,9 +331,13 @@ static uint8_t get_sprite_pixel(uint8_t sprite_row, uint8_t col) {
   return sprite_row & (0x80u >> col);
 }
 
-static
-
-void op_Dxyn(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Display n-byte sprite starting at memory location I at (Vx, Vy), set
+ * VF = collision.
+ */
+static void op_Dxyn(chip8_t *chip8) {
   // reset collision bit
   chip8->registers[0xF] = 0;
 
@@ -239,7 +351,7 @@ void op_Dxyn(chip8_t *chip8) {
   for (uint8_t row = 0; row < sprite.sprite_height; ++row) {
     uint8_t sprite_row = chip8->memory[chip8->index + row];
     for (uint8_t col = 0; col < 8; ++col) {
-      uint8_t sprite_pixel  = get_sprite_pixel(sprite_row, col);
+      uint8_t sprite_pixel = get_sprite_pixel(sprite_row, col);
       uint32_t *video_pixel = get_video_pixel(chip8, &sprite, row, col);
       // Sprite pixel is on
       if (sprite_pixel) {
@@ -254,7 +366,12 @@ void op_Dxyn(chip8_t *chip8) {
   }
 }
 
-void op_Ex9E(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Skip next instruction if key with the value of Vx is pressed.
+ */
+static void op_Ex9E(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t key = chip8->registers[vx];
 
@@ -262,7 +379,12 @@ void op_Ex9E(chip8_t *chip8) {
     chip8->pc += 2;
 }
 
-void op_ExA1(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Skip next instruction if key with the value of Vx is not pressed.
+ */
+static void op_ExA1(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t key = chip8->registers[vx];
 
@@ -270,12 +392,22 @@ void op_ExA1(chip8_t *chip8) {
     chip8->pc += 2;
 }
 
-void op_Fx07(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Skip next instruction if key with the value of Vx is not pressed.
+ */
+static void op_Fx07(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   chip8->registers[vx] = chip8->delay_timer;
 }
 
-void op_Fx0A(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Wait for a key press, store the value of the key in Vx.
+ */
+static void op_Fx0A(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   if (chip8->keypad[0]) {
     chip8->registers[vx] = 0;
@@ -314,29 +446,57 @@ void op_Fx0A(chip8_t *chip8) {
   }
 }
 
-void op_Fx15(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set delay timer = vx.
+ */
+static void op_Fx15(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   chip8->delay_timer = chip8->registers[vx];
 }
 
-void op_Fx18(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set sound timer = vx.
+ */
+static void op_Fx18(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   chip8->sound_timer = chip8->registers[vx];
 }
 
-void op_Fx1E(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set I = I + Vx.
+ */
+static void op_Fx1E(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   chip8->index += chip8->registers[vx];
 }
 
-void op_Fx29(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Set I = location of sprite for digit Vx.
+ */
+static void op_Fx29(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t digit = chip8->registers[vx];
 
   chip8->index = static_cast<uint16_t>(FONTSET_START_ADDRESS + (5 * digit));
 }
 
-void op_Fx33(chip8_t *chip8) {
+/**
+ * @ingroup table
+ *
+ * @brief Store BCD representation of Vx in memory locations I, I+1, and I+2.
+ * The interpreter takes the decimal value of Vx, and places the hundreds digit
+ * in memory at location in I, the tens digit at location I+1, and the ones
+ * digit at location I+2.
+ */
+static void op_Fx33(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   uint8_t value = chip8->registers[vx];
 
@@ -349,40 +509,40 @@ void op_Fx33(chip8_t *chip8) {
   chip8->memory[chip8->index] = value % 10;
 }
 
-void op_Fx55(chip8_t *chip8) {
+static void op_Fx55(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   for (uint8_t i = 0; i <= vx; ++i)
     chip8->memory[chip8->index + i] = chip8->registers[i];
 }
 
-void op_Fx65(chip8_t *chip8) {
+static void op_Fx65(chip8_t *chip8) {
   uint8_t vx = make_vx(chip8->opcode);
   for (uint8_t i = 0; i <= vx; ++i) {
     chip8->registers[i] = chip8->memory[chip8->index + i];
   }
 }
 
-void opcodes_0(chip8_t *chip8) {
+static void opcodes_0(chip8_t *chip8) {
   auto instruction = table0[chip8->opcode & 0x000Fu];
   instruction(chip8);
 }
 
-void opcodes_8(chip8_t *chip8) {
+static void opcodes_8(chip8_t *chip8) {
   auto instruction = table8[chip8->opcode & 0x000Fu];
   instruction(chip8);
 }
 
-void opcodes_E(chip8_t *chip8) {
+static void opcodes_E(chip8_t *chip8) {
   auto instruction = tableE[chip8->opcode & 0x000Fu];
   instruction(chip8);
 }
 
-void opcodes_F(chip8_t *chip8) {
+static void opcodes_F(chip8_t *chip8) {
   auto instruction = tableF[chip8->opcode & 0x00FFu];
   instruction(chip8);
 }
 
-void op_null([[maybe_unused]] chip8_t *chip8) {}
+static void op_null([[maybe_unused]] chip8_t *chip8) {}
 
 static void init_dispatch_table() {
   dispatch_table[0x0] = opcodes_0;
